ce77eb73ad05478476ff05416c8c3ae3294bc64f
Arick Vigas <yaroslav.vyhivskyi@globallogic.com>
2021-08-18 05:36:10 +0300

    Build v06

---
+++ dist/scenes/Menu.d.ts
@@ -0,0 +1,5 @@
+import { Container } from 'pixi.js';
+export default class MenuScene extends Container {
+    constructor();
+    private generateButtons;
+}
---
+++ dist/utils/TextStylesAdvisor.d.ts
@@ -0,0 +1,2 @@
+import { TextStyle } from 'pixi.js';
+export default function TextStylesAdvisor(): TextStyle;
